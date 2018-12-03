a2be617dce651ff0139c862afb6ae7956b956973
Tyler Leonhardt <tylerl0706@gmail.com>
2018-12-02 17:37:04 -0800

    day2 part1

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # Advent of Code 2018
 
-Join my private leaderboard! Enter this code on the leaderboard page: `31108-50673fef`.
+Join my private leaderboard!
+Enter this code on the leaderboard page:
+`31108-50673fef`.
 
 The reality of me finishing this is low...
 but Advent of Code is a great opportunity to get more familiar with a language.
 
-This year, I've chosen to use `TypeScript`.
+This year,
+I've chosen to use
+`TypeScript`.
+
+## Getting Started
+
+To run the code,
+first make sure you have Node.js installed.
+Then,
+open up a terminal and run:
+
+```javascript
+npm install
+```
+
+From here,
+you can run
+`npm start`
+with the days you want to run:
+
+```sh
+# Runs Day 1's parts
+npm start 1
+npm start Day1
+
+# Runs Day 1 and 2's parts
+npm start 1 2
+npm start Day1 Day2
+```
+
+The days are each a TypeScript class that extends a base AdeventOfCodeDay class.
+It's a bit over-engineered for Advent,
+but it was a good opportunity to refresh my OOP TypeScript skills.
--- src/Day2/Day2.ts
+++ src/Day2/Day2.ts
@@ -3,9 +3,42 @@ import { AdventOfCodeDay } from "../AdventOfCodeDay";
 
 export class Day2 extends AdventOfCodeDay {
     public part1(): void {
-        this.logAnswer("Day2", "part1", "not implemented");
+        const input: string[] = this.getInput("Day2", "part1");
+        let numOfTwos: number = 0;
+        let numOfThrees: number = 0;
+
+        for (const item of input) {
+            let hasTwo: boolean = false;
+            let hasThree: boolean = false;
+            const freq = this.getCharacterFrequencyMatrix(item);
+
+            for (const kvp of freq) {
+                if (kvp[1] === 2) {
+                    hasTwo = true;
+                } else if (kvp[1] === 3) {
+                    hasThree = true;
+                }
+            }
+
+            numOfTwos += hasTwo ? 1 : 0;
+            numOfThrees += hasThree ? 1 : 0;
+        }
+
+        this.logAnswer("Day2", "part1", numOfTwos * numOfThrees);
     }
 
+    private getCharacterFrequencyMatrix(str: string): Map<string, number> {
+        const freq = new Map<string, number>();
+        for (const char of str) {
+            if (!freq.has(char)) {
+                freq.set(char, 0);
+            }
+            freq.set(char, freq.get(char) + 1);
+        }
+        return freq;
+    }
+
+    // tslint:disable-next-line:member-ordering
     public part2(): void {
         this.logAnswer("Day2", "part2", "not implemented");
     }
